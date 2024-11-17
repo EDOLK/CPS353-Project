72172991e24902138764f149bf885328170febb1
kyliedelamater <kyliedelamater@gmail.com>
2024-11-16 19:23:08 -0500

    test to make sure factorialSum works

---
+++ test/FactorialTest.java
@@ -0,0 +1,43 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import interfaces.NumStream;
+import mainpack.ComputationImplementation;
+import mainpack.EngineResponse;
+import mainpack.RequestResult;
+import mainpack.ResponseCode;
+
+public class FactorialTest {
+
+	@Test
+	public void testDoFactorial() {
+	    NumStream numStream = new NumStream() {
+	        @Override
+	        public List<Integer> getIntegers() {
+	            return Arrays.asList(4, 5, 6);   //adds 4 5 6 into the list
+	        }
+			@Override
+			public void setIntegerList(List<Integer> integerList) {
+				return;
+			}
+	    };
+
+	    // 4! = 24   5! = 120   6! = 720
+	    List<Integer> expectedResults = Arrays.asList(6, 3, 9);
+	    
+	    ComputationImplementation computation = new ComputationImplementation();
+	    EngineResponse response = computation.doFactorialSum(numStream);
+	    assertEquals(ResponseCode.SUCCESSFUL, response.getResponseCode());
+	    
+	    RequestResult requestResult = response.getRequestResult();
+	    NumStream resultNumStream = requestResult.getResultNumStream();
+	    
+	    List<Integer> actualResults = resultNumStream.getIntegers();
+	    assertEquals(expectedResults, actualResults);
+	}
+}
